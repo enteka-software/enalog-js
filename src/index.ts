0389741cfa0858919824ce840d5a8798003eebbe
JosephO1992 <josephraoreilly@gmail.com>
2023-02-27 07:33:14 +0000

    wip: testing set up (not working importing modules into the test), made some progress on responses

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,37 @@ export type Event = {
 };
 
 export async function pushEvent(apiToken: String, data: Event) {
-  const res = await fetch("http://127.0.0.1:3000/v1/events", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer: ${apiToken}`,
-    },
-    body: JSON.stringify(data),
-  });
-  
-  return res
+
+  try {
+
+    if (!apiToken) {
+      throw new Error('No api token provided')
+    }
+
+    if (!data) {
+      throw new Error('No data provided')
+    }
+
+    const res = await fetch("http://127.0.0.1:3000/v1/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer: ${apiToken}`,
+      },
+      body: JSON.stringify(data),
+
+    })
+
+    if (res.status === 200) {
+      return res
+    }
+
+    if (res.status !== 200) {
+      return Promise.reject(res)
+    }
+
+  } catch (error) {
+    return new Error("error:" + error);
+  }
+
 }
---
+++ test/index.test.ts
@@ -0,0 +1,11 @@
+// import { pushEvent } from "./src/index"
+import { describe, expect, test } from '@jest/globals';
+
+
+// describe('pushEvent', () => {
+//     test('it returns an error if no api token is provided', () => {
+//         let response = pushEvent();
+
+
+//     })
+// })
